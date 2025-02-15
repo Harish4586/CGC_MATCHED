451ee1790e91868f57a47d759886c1fd6ace4594
Harish <nirbaanharish@gmail.com>
2025-02-15 21:44:04 +0530

    final submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-import Body from "./Body"
-import Login from "./Login"
-import Profile from "./Profile"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
+import Premium from "./components/Premium";
+import Chat from "./components/Chat";
 
 function App() {
- 
-
   return (
     <>
-      <BrowserRouter basename="/">
-      <Routes>
-      {/* <Route path="/" element={<div>Base page</div>} /> by this way, we can't create children Routes */}
-      {/* children routes in body component */}
-    <Route path="/" element={<Body/>}>
-    <Route path="/login" element={<Login/>}/>
-    <Route path="/profile" element={<Profile/>}/>
-    
-    </Route>
-      </Routes>
-      
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/premium" element={<Premium />} />
+              <Route path="/chat/:targetUserId" element={<Chat />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
